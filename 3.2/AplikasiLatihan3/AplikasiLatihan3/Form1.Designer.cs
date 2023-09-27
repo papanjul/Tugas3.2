3214ebc0c43727a2862e8d03a5ba9144b90d8f3a
papanjul <122225116+papanjul@users.noreply.github.com>
2023-09-27 18:28:00 +0700

    Add files via upload

---
+++ 3.2/AplikasiLatihan3/AplikasiLatihan3/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace AplikasiLatihan3
+{
+    partial class frmLatihanTiga3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.cmbPendidikan = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstPekerjaan = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pendidikan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pekerjaan";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(86, 19);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(341, 20);
+            this.txtNama.TabIndex = 3;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.Enabled = false;
+            this.txtPesan1.Location = new System.Drawing.Point(2, 10);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(403, 20);
+            this.txtPesan1.TabIndex = 4;
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.Enabled = false;
+            this.txtPesan2.Location = new System.Drawing.Point(3, 6);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(402, 20);
+            this.txtPesan2.TabIndex = 5;
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.Enabled = false;
+            this.txtPesan3.Location = new System.Drawing.Point(17, 245);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(403, 20);
+            this.txtPesan3.TabIndex = 6;
+            this.txtPesan3.TextChanged += new System.EventHandler(this.txtPesan3_TextChanged);
+            // 
+            // cmbPendidikan
+            // 
+            this.cmbPendidikan.FormattingEnabled = true;
+            this.cmbPendidikan.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "DIPLOMA",
+            "SARJANA",
+            "PASCASARJANA"});
+            this.cmbPendidikan.Location = new System.Drawing.Point(85, 43);
+            this.cmbPendidikan.Name = "cmbPendidikan";
+            this.cmbPendidikan.Size = new System.Drawing.Size(341, 21);
+            this.cmbPendidikan.TabIndex = 7;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(2, 11);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(401, 23);
+            this.btnOk.TabIndex = 9;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nama";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(74, 153);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // lstPekerjaan
+            // 
+            this.lstPekerjaan.FormattingEnabled = true;
+            this.lstPekerjaan.Items.AddRange(new object[] {
+            "PEDAGANG",
+            "PNS",
+            "DOSEN",
+            "SWASTA",
+            "ABRI"});
+            this.lstPekerjaan.Location = new System.Drawing.Point(6, 17);
+            this.lstPekerjaan.Name = "lstPekerjaan";
+            this.lstPekerjaan.Size = new System.Drawing.Size(322, 82);
+            this.lstPekerjaan.TabIndex = 8;
+            this.lstPekerjaan.SelectedIndexChanged += new System.EventHandler(this.lstPekerjaan_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPesan1);
+            this.groupBox2.Location = new System.Drawing.Point(15, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(408, 36);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtPesan2);
+            this.groupBox3.Location = new System.Drawing.Point(14, 210);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(406, 29);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.lstPekerjaan);
+            this.groupBox5.Location = new System.Drawing.Point(86, 70);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(334, 108);
+            this.groupBox5.TabIndex = 15;
+            this.groupBox5.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnOk);
+            this.groupBox4.Location = new System.Drawing.Point(17, 271);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(402, 34);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // frmLatihanTiga3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 311);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.txtPesan3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbPendidikan);
+            this.Controls.Add(this.txtNama);
+            this.Name = "frmLatihanTiga3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan 3.3";
+            this.Load += new System.EventHandler(this.frmLatihanTiga3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan3;
+        private System.Windows.Forms.ComboBox cmbPendidikan;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstPekerjaan;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
